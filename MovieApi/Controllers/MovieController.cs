51155181ab748f125761b72476d66b66d5ff97db
Igor Nascimento Medeiros <nascimentoigor49@gmail.com>
2022-06-05 19:07:00 -0300

    Add project files.

---
+++ MovieApi/Controllers/MovieController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Models;
+
+namespace MovieApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static List<Movie> movies = new List<Movie>();
+        private static int id = 1;
+
+        [HttpPost]
+        public IActionResult AddMovie(Movie movie)
+        {
+            movie.Id = id++;
+            movies.Add(movie);
+            return CreatedAtAction(nameof(GetMovieById), new {Id = movie.Id}, movie);
+        }
+
+        [HttpGet]
+        public IActionResult GetMovies()
+        {
+            return Ok(movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetMovieById(int id)
+        {
+            var movie = movies.FirstOrDefault(movie => movie.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(movie);
+            }
+        }
+    }
+}
---
+++ MovieApi/Models/Movie.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieApi.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        [Required(ErrorMessage = "Title can't be null or empty")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Director can't be null or empty")]
+        public string Director { get; set; }
+        public string Genre { get; set; }
+        [Range(60, 600, ErrorMessage = "Duration can't be less than 60 and can't be more than 600 minutes")]
+        public int Duration { get; set; }
+    }
+}
