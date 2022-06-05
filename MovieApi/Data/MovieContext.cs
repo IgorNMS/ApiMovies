a4dc757fc5283654a1637f04e2ec7701e0be6b0d
Igor Nascimento Medeiros <nascimentoigor49@gmail.com>
2022-06-05 19:28:29 -0300

    gitignore atualizado com contexto para conexão ao DB criado.

---
+++ MovieApi/Data/MovieContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MovieApi.Models;
+
+namespace MovieApi.Data
+{
+    public class MovieContext : DbContext
+    {
+        public MovieContext(DbContextOptions<MovieContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
