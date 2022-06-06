0d8ee8bc859517b809f5203e597a574d7c944659
Igor Nascimento Medeiros <nascimentoigor49@gmail.com>
2022-06-05 21:22:36 -0300

    Conexão com DB feita.

--- MovieApi/Controllers/MovieController.cs
+++ MovieApi/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MovieApi.Data;
 using MovieApi.Models;
 
 namespace MovieApi.Controllers
@@ -7,27 +8,31 @@ namespace MovieApi.Controllers
     [Route("[controller]")]
     public class MovieController : ControllerBase
     {
-        private static List<Movie> movies = new List<Movie>();
-        private static int id = 1;
+        private MovieContext _context;
+
+        public MovieController(MovieContext context)
+        {
+            _context = context;
+        }
 
         [HttpPost]
         public IActionResult AddMovie(Movie movie)
         {
-            movie.Id = id++;
-            movies.Add(movie);
+            _context.Movies.Add(movie);
+            _context.SaveChanges(); 
             return CreatedAtAction(nameof(GetMovieById), new {Id = movie.Id}, movie);
         }
 
         [HttpGet]
-        public IActionResult GetMovies()
+        public IEnumerable<Movie> GetMovies()
         {
-            return Ok(movies);
+            return _context.Movies;
         }
 
         [HttpGet("{id}")]
         public IActionResult GetMovieById(int id)
         {
-            var movie = movies.FirstOrDefault(movie => movie.Id == id);
+            var movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
             if (movie == null)
             {
                 return NotFound();
@@ -36,6 +41,35 @@ namespace MovieApi.Controllers
             {
                 return Ok(movie);
             }
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateMovie(int id, [FromBody] Movie newMovie)
+        {
+            Movie movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            movie.Title = newMovie.Title;
+            movie.Director = newMovie.Director;
+            movie.Genre = newMovie.Genre;
+            movie.Duration = newMovie.Duration;
+            _context.SaveChanges();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMovie(int id)
+        {
+            Movie movie = _context.Movies.FirstOrDefault(movie => movie.Id == id);
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(movie);
+            _context.SaveChanges();
+            return NoContent();
         }
     }
 }
--- MovieApi/Data/MovieContext.cs
+++ MovieApi/Data/MovieContext.cs
@@ -5,11 +5,18 @@ namespace MovieApi.Data
 {
     public class MovieContext : DbContext
     {
-        public MovieContext(DbContextOptions<MovieContext> opt) : base(opt)
+        public MovieContext(DbContextOptions<MovieContext> options) : base(options)
         {
 
         }
 
         public DbSet<Movie> Movies { get; set; }
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    //Para pegar a connection do appsettings.json
+        //    IConfigurationRoot configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory).AddJsonFile("appsettings.json").Build();
+        //    optionsBuilder.UseMySQL(configuration.GetConnectionString("MovieConnection"));
+        //}
     }
 }
